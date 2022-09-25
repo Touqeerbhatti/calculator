58b78de5d71bacabec14680dd43d54a7e77bfcf0
Touqeer Ahmed <badsha14072@gmail.com>
2022-09-25 12:11:04 -0700

    complete

--- src/App.js
+++ src/App.js
@@ -1,48 +1,71 @@
 
-
-import { getByDisplayValue } from "@testing-library/react";
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
+import './bootstrap.css';
 
 
 const App= ()=>
-{
+{ 
+  const [value,setvalue]=useState();
 
+  const handleClick =(e)=>
+  {
+    setvalue(value.concat(e.target.defaultValue))
+  }  
+  const Clear = ()=>
+  {
+    setvalue("")
+  }
+  const backSpace=()=>
+  {
+    setvalue(value.slice(0, -1))
+  }
+  const calculate=()=>{
+    try{
+        setvalue(eval(value).toString())
+    }
+    catch(err){
+      setvalue("Error")
+
+    }
+  }
 
   return(
       <>
+      <div className="container">
         <div className="main-div">
           <div className="sub-div">
             <div className="input-div">
-          <input className="input-tag" type="text"   />
+          <input className="input-tag" type="text" Value={value}  />
             </div>
 
             <div className="btn-div">
               
-              <input type="button" className="btn" defaultValue={0} onClick="display('0')"  />
-              <input type="button" className="btn" defaultValue="[" onClick="display('[')"  />
-              <input type="button" className="btn" defaultValue="]" onClick="display(']')"  />
-              <input type="button" className="btn" defaultValue={"C"} onClick="clear('C')"  />
-              <input type="button" className="btn" defaultValue={7} onClick="display('7')"  />
-              <input type="button" className="btn" defaultValue={8} onClick="display('8')"  />
-              <input type="button" className="btn" defaultValue={9} onClick="display('9')"  />
-              <input type="button" className="btn" defaultValue="-" onClick="sub('-')"  />
-              <input type="button" className="btn" defaultValue={4} onClick="display('4')"  />
-              <input type="button" className="btn" defaultValue={5} onClick="display('5')"  />
-              <input type="button" className="btn" defaultValue={6} onClick="display('6')"  />
-              <input type="button" className="btn" defaultValue="\" onClick="div('\')"  />
-              <input type="button" className="btn" defaultValue={1} onClick="display('1')"  />
-              <input type="button" className="btn" defaultValue={2} onClick="display('2')"  />
-              <input type="button" className="btn" defaultValue={3} onClick="display('3')"  />
-              <input type="button" className="btn" defaultValue="*" onClick="mul('*')"  />
-              <input type="button" className="btn" defaultValue={0} onClick="display('0')"  />
-              <input type="button" className="btn" defaultValue="." onClick="display('.')"  />
-              <input type="button" className="btn" defaultValue="+" onClick="add('+')"  />
-              <input type="button" className="btn" defaultValue="=" onClick="equal('=')"  />
+              <input type="button" className="btn" defaultValue={"Clear"} onClick={Clear}  />
+              <input type="button" className="btn" defaultValue={"["} onClick={handleClick}  />
+              <input type="button" className="btn" defaultValue={"]"} onClick={handleClick}  />
+              <input type="button" className="btn" defaultValue={"C"} onClick={backSpace}  />
+              <input type="button" className="btn" defaultValue={7} onClick={handleClick}  />
+              <input type="button" className="btn" defaultValue={8} onClick={handleClick}  />
+              <input type="button" className="btn" defaultValue={9} onClick={handleClick}  />
+              <input type="button" className="btn" defaultValue={"-"} onClick={handleClick}  />
+              <input type="button" className="btn" defaultValue={4} onClick={handleClick}  />
+              <input type="button" className="btn" defaultValue={5} onClick={handleClick}  />
+              <input type="button" className="btn" defaultValue={6} onClick={handleClick}  />
+              <input type="button" className="btn" defaultValue="/" onClick={handleClick}  />
+              <input type="button" className="btn" defaultValue={1} onClick={handleClick}  />
+              <input type="button" className="btn" defaultValue={2} onClick={handleClick}  />
+              <input type="button" className="btn" defaultValue={3} onClick={handleClick}  />
+              <input type="button" className="btn" defaultValue="*" onClick={handleClick}  />
+              <input type="button" className="btn" defaultValue={0} onClick={handleClick}  />
+              <input type="button" className="btn" defaultValue="." onClick={handleClick}  />
+              <input type="button" className="btn" defaultValue="+" onClick={handleClick}  />
+              <input type="button" className="btn" defaultValue="=" onClick={calculate}    />
             
             </div>
 
           </div>
+        </div>
         </div>
 
       </>
